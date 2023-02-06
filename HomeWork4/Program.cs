97d43fe7c7d048170770a84cf8fa9743852da9e8
Replit user <>
2023-02-06 22:23:59 +0000

    HomeWork4 is done

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -4,7 +4,7 @@ bool isWork = true;
 while (isWork)
 {
   Console.Clear();
-  Console.Write("Какое задание вы хотите проверить?\nВведите целое число от 1 до 4\nИли напишите /h для справки\n\nВаш выбор: ");
+  Console.Write("Какое задание вы хотите проверить?\nВведите целое число от 1 до 3\nИли напишите /h для справки\n\nВаш выбор: ");
 
   var word = Console.ReadLine();
   if (word == "1" || word == "2" || word == "3")
@@ -33,7 +33,7 @@ while (isWork)
 
           Console.Write("Вы выбрали задачу номер 2\nВывод суммы цифр числа.\n\nВведите число: ");
           int number = ReadInt();
-          Console.WriteLine($"Сумма всех цифр в числе {number}: {}");
+          Console.WriteLine($"Сумма всех цифр в числе {number}: {CalculateSumOfDigits(number)}");
 
           Console.Write("\nНажмите любую кнопку для возврата в главное меню");
           Console.ReadKey();
@@ -43,9 +43,13 @@ while (isWork)
       case 3: //Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
         {
           Console.Clear();
-          Console.Write("Вы выбрали задачу номер 3\nВывод массива из 8 элементов.\n\nВведите число: ");
+          Console.Write("Вы выбрали задачу номер 3\nВывод массива из 8 элементов.\n\nНажмите любую кнопку для вывода массива");
+          Console.ReadKey();
 
-          
+          int[] array = GetArrayOfInts(8);
+          FillArray(array);
+          Console.Write($"Ваш случайный массив: ");
+          PrintArray(array);
 
           Console.Write("\nНажмите любую кнопку для возврата в главное меню");
           Console.ReadKey();
@@ -61,7 +65,7 @@ while (isWork)
   else if (word.ToLower() == "/help" || word.ToLower() == "/h")
   {
     Console.Clear();
-    Console.Write("Справка:\n1. Вывод наибольшего из двух чисел\n2. Вывод наибольшего из трёх чисел\n3. Проверка чётности числа\n4. Вывод всех чётных чисел от 1 до введённого включительно\n/help. Справка\nExit или E. Завершение работы программы\n\nДля возврата в главное меню нажмите любую кнопку");
+    Console.Write("Справка:\n1. Вывод результата возведения числа А в степень В.\n2. Вывод суммы цифр числа.\n3. Вывод массива из 8 элементов.\n/help. Справка\nExit или E. Завершение работы программы\n\nДля возврата в главное меню нажмите любую кнопку");
     Console.ReadKey();
   }
   else if (word.ToLower() == "exit" || word.ToLower() == "e" || word.ToLower() == "у")
@@ -96,6 +100,47 @@ int CalculateSumOfDigits(int number)
 {
   int sumOfDigits = 0;
 
+  while (number > 0)
+  {
+    sumOfDigits += number % 10;
+    number /= 10;
+  }
+
+  return sumOfDigits;
+}
+
+// Методы для третьей задачи
+
+int[] GetArrayOfInts(int count)
+{
+  int[] array = new int[count];
+
+  return array;
+}
+
+void FillArray(int[] array)
+{
+  Random random = new Random();
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = random.Next(0, 100);
+  }
+}
+
+void PrintArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (i < array.Length - 1)
+    {
+      Console.Write($"{array[i]}, ");
+    }
+    else
+    {
+      Console.Write($"{array[i]}.");
+    }
+  }
 }
 
 // Общие методы
