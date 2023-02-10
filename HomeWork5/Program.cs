3d95d0a753e6404cef4d911e773e507a75bfb80f
DanilAiro <91245260+DanilAiro@users.noreply.github.com>
2023-02-10 14:41:38 +0300

    HomeWork5 is done

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -1,10 +1,10 @@
 bool isWork = true;
 Console.Clear();
-string mainMenu = $"Если хотите вызвать справку, напишите - /help.{Environment.NewLine}Если хотите завершить работу программы, напишите - exit.{Environment.NewLine}Если хотите очистить терминал, напишите clear.{Environment.NewLine}{Environment.NewLine}Какую задачу хотите проверить?{Environment.NewLine}Напишите номер задачи от 1 до 3: ";
+string mainMenuText = $"Если хотите вызвать справку, напишите - /help.{Environment.NewLine}Если хотите завершить работу программы, напишите - exit.{Environment.NewLine}Если хотите очистить терминал, напишите clear.{Environment.NewLine}{Environment.NewLine}Какую задачу хотите проверить?{Environment.NewLine}Напишите номер задачи от 1 до 3: ";
 
 while (isWork)
 {
-  Console.Write(mainMenu);
+  Console.Write(mainMenuText);
   
   var word = Console.ReadLine();
   Console.WriteLine();
@@ -16,59 +16,62 @@ while (isWork)
       case "1":
         {
           Task1_CountOfEven();
-          WantToEndTask();
+          ToEndTask();
           break;
         }
-
       case "2": 
         {
           Task2_SumOfNotEvenIndexes();
-          WantToEndTask();
+          ToEndTask();
           break;
         }
       case "3":
         {
           Task3_DifferenceBetweenMinAndMax();
-          WantToEndTask();
+          ToEndTask();
           break;
         }
     }
   }
   else if (word.ToLower() == "e" || word.ToLower() == "exit")
   {
-      WantToExit();
+    isWork = false;
   }
   else if (word.ToLower() == "c" || word.ToLower() == "clear")
   {
-      WantToClear();
+    Console.Clear();
   }
   else if (word.ToLower() == "/help" || word.ToLower() == "h")
   {
-      
+    ToHelp();
   }
   else
   {
-    
+    Console.WriteLine($"Команда не была распознана, повторите ввод{Environment.NewLine}");
   }
   
 }
 
 #region Methods_for_work
 
-void WantToClear()
-{
-  Console.Clear();
-}
-
-void WantToExit()
+void ToExit()
 {
   isWork = false;
 }
 
-void WantToEndTask()
+void ToEndTask()
 {
-  Console.Write($"{Environment.NewLine}Для возврата в главное меню, нажмите любую кнопку");
+  Console.WriteLine($"{Environment.NewLine}Для возврата в главное меню, нажмите любую кнопку{Environment.NewLine}");
   Console.ReadKey();
+}
+
+void ToHelp()
+{
+  string text = $"Справка:{Environment.NewLine}1. Вывод результата возведения числа А в степень В.{Environment.NewLine}2. Вывод суммы цифр числа.{Environment.NewLine}3. Вывод массива из 8 элементов.{Environment.NewLine}/help или /h. Справка{Environment.NewLine}Exit или E. Завершение работы программы";
+
+  Console.Clear();
+  Console.WriteLine(text);
+  ToEndTask();
 }
 
 #endregion
@@ -160,31 +163,37 @@ int CalculateDifference(int firstNumber, int secondNumber)
 
 void Task1_CountOfEven()
 {
+  string text = "Вы выбрали задачу на подсчёт количества чётных чисел в массиве";
+  Console.WriteLine(text);
   int[] array = GetRandomArray(ReadInt("длину массива"), ReadInt("минимальное значение наполнения"), ReadInt("максимальное значение наполнения"));
   int count = CalculateCountOfEven(array);
 
   if (count != 0)
-    Console.WriteLine($"В вашем массиве {string.Join(", ", array)} обнаружилось {count} чётных чисел.");
+    Console.WriteLine($"В вашем массиве [{string.Join(", ", array)}] обнаружилось {count} чётных чисел.");
   else
-    Console.WriteLine($"В вашем массиве {string.Join(", ", array)} необнаружились чётные числа.");
+    Console.WriteLine($"В вашем массиве [{string.Join(", ", array)}] необнаружились чётные числа.");
 }
 
 void Task2_SumOfNotEvenIndexes()
 {
+  string text = "Вы выбрали задачу на подсчёт суммы чисел стоящих на нечётных индексах в массиве";
+  Console.WriteLine(text);
   int[] array = GetRandomArray(ReadInt("длину массива"), ReadInt("минимальное значение наполнения"), ReadInt("максимальное значение наполнения"));
   int sum = CalculateSumOfNotEvenIndexes(array);
 
   if (sum != 0)
-    Console.WriteLine($"Сумма всех элементов стоящих на нечётных индексах вашего массива {string.Join(", ", array)} равна {sum}.");
+    Console.WriteLine($"Сумма всех элементов стоящих на нечётных индексах вашего массива [{string.Join(", ", array)}] равна {sum}.");
 }
 
 void Task3_DifferenceBetweenMinAndMax()
 {
+  string text = "Вы выбрали задачу на подсчёт разницы между минимальным и максимальным значениями массива";
+  Console.WriteLine(text);
   int[] array = GetRandomArray(ReadInt("длину массива"), ReadInt("минимальное значение наполнения"), ReadInt("максимальное значение наполнения"));
   int[] minAndMax = FindMinAndMax(array);
   int difference = CalculateDifference(minAndMax[0], minAndMax[1]);
 
-  Console.WriteLine($"Разница между минимальным и максимальным значение вашего массива {string.Join(", ", array)} равна {difference}");
+  Console.WriteLine($"Разница между минимальным и максимальным значение вашего массива [{string.Join(", ", array)}] равна {difference}");
 }
 
 #endregion
